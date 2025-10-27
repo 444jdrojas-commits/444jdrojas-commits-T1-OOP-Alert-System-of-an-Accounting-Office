e70ccef39b278746ecb1cc5a9d2900fc82ff431d
PauloRamos24 <paulo.alramos24@gmail.com>
2025-10-26 22:20:25 -0500

    v0.1.1

---
+++ 06Code/SystemAlert/src/main/java/com/mycompany/systemalert/SystemAlert.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.systemalert;
+
+/**
+ *
+ * @author Paulo Ramos
+ */
+public class SystemAlert {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
