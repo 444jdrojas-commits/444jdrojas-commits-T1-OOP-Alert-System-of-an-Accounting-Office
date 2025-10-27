2866d192a65a68bace0fcf848bd64fdc0df41758
PauloRamos24 <paulo.alramos24@gmail.com>
2025-10-26 23:00:58 -0500

    v0.1.6

---
+++ 06Code/SystemAlert/src/main/java/ec/edu/espe/AlertSystemOfAccountingOffice/model/Invoice.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.AlertSystemOfAccountingOffice.model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Paulo Ramos
+ */
+public class Invoice {
+    
+    private int invoiceNumber;
+    private Date paymentDate;
+    private float amountPaid;
+    private String details;
+    private String status;
+    
+}
