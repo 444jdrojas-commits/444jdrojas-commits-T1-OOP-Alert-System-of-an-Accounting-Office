c2f809ce03b633eaae59b7ab70dd8faeb951dddb
Josue David Rojas Peña <svp.jd.rojas@gmail.com>
2025-10-26 23:03:05 -0500

    v 0.1.7

---
+++ 06Code/SystemAlert/src/main/java/ec/edu/espe/AlertSystemOfAccountingOffice/model/Document.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.AlertSystemOfAccountingOffice.model;
+
+/**
+ *
+ * @author JOSUE
+ */
+public class Document {
+    private String typeDocument;
+    private String status;
+    private String name;
+    private Date reviewDay;
+    private String details;
+}
