0571fac6b116e73fa37d8a28e25a8a6d1b1ffe10
Josue David Rojas Peña <svp.jd.rojas@gmail.com>
2025-10-26 23:06:13 -0500

    v 0.1.8

---
+++ 06Code/SystemAlert/src/main/java/ec/edu/espe/AlertSystemOfAccountingOffice/model/Rol.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.AlertSystemOfAccountingOffice.model;
+
+/**
+ *
+ * @author JOSUE
+ */
+public class Rol {
+    private String rol;
+    private String name;
+    private String description;
+}
