2c625f7a596898d942503159ad55ae67bf23a6cb
Josue David Rojas Peña <svp.jd.rojas@gmail.com>
2025-10-26 22:58:18 -0500

    v 0.1.5

---
+++ 06Code/SystemAlert/src/main/java/ec/edu/espe/AlertSystemOfAccountingOffice/model/Business.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.AlertSystemOfAccountingOffice.model;
+
+/**
+ *
+ * @author JOSUE
+ */
+public class Business {
+    private String nameBusiness;
+    private String legalRepresentative;
+    private String typeBusiness;
+    private String address;
+    private String phone;
+    private String email;
+    private String ruc;
+}
