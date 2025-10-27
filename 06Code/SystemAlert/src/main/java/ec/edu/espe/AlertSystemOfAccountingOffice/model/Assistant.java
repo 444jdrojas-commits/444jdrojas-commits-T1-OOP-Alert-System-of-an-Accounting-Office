5144cc01a920d3325a3b722f7cc1a6f2be90bf78
PauloRamos24 <paulo.alramos24@gmail.com>
2025-10-26 22:55:41 -0500

    v0.1.4

---
+++ 06Code/SystemAlert/src/main/java/ec/edu/espe/AlertSystemOfAccountingOffice/model/Assistant.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.AlertSystemOfAccountingOffice.model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Paulo Ramos
+ */
+public class Assistant {
+    
+    private String name;
+    private Date birthDate;
+    private int phone;
+    private String email;
+    private String user;
+    private String password;
+}
