dd593e18f0b98c0f856b367905c50d1f31ef25ee
PauloRamos24 <paulo.alramos24@gmail.com>
2025-10-26 22:42:25 -0500

    v0.1.3

---
+++ 06Code/SystemAlert/src/main/java/ec/edu/espe/AlertSystemOfAccountingOffice/model/NaturalPerson.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.AlertSystemOfAccountingOffice.model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Paulo Ramos
+ */
+public class NaturalPerson {
+    
+    private String name;
+    private String identification;
+    private String nationality;
+    private Date birthDate;
+    private String phone;
+    private String email;
+    private String ocuppation;
+    private String gender;
+    
+    
+    
+}
