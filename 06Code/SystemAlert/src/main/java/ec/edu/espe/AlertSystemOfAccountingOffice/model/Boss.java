29d3d4ea2170471f9664fb40c109864eb4ae7e76
Josue David Rojas Peña <svp.jd.rojas@gmail.com>
2025-10-26 22:40:42 -0500

    v 0.1.2

---
+++ 06Code/SystemAlert/src/main/java/ec/edu/espe/AlertSystemOfAccountingOffice/model/Boss.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.AlertSystemOfAccountingOffice.model;
+
+/**
+ *
+ * @author JOSUE
+ */
+public class Boss {
+    private String name;
+    private Date birthdate;
+    private int phone;
+    private String email;
+    private String user;
+    private String password;
+}
