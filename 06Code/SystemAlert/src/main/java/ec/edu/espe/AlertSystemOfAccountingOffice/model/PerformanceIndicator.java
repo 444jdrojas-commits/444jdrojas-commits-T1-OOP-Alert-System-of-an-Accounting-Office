519e185ef8176185aa7b5e0fc50b9ebd919a9623
Josue David Rojas Peña <svp.jd.rojas@gmail.com>
2025-10-26 22:53:34 -0500

    v 0.1.3

---
+++ 06Code/SystemAlert/src/main/java/ec/edu/espe/AlertSystemOfAccountingOffice/model/PerformanceIndicator.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.AlertSystemOfAccountingOffice.model;
+
+/**
+ *
+ * @author JOSUE
+ */
+public class PerformanceIndicator {
+    private String period;
+    private Assitant assistant;
+    private double completionRate;
+    private double averageDelay;
+    private double efficiencyPerDay;
+    private double finalScore;
+    private String comments;
+    
+}
