906d2d4f4ba6290a4719b2eaed012ca0f181770a
PauloRamos24 <paulo.alramos24@gmail.com>
2025-11-02 22:06:40 -0500

    v0.1.10

--- 06Code/SystemAlert/src/main/java/ec/edu/espe/AlertSystemOfAccountingOffice/model/Addres.java
+++ 06Code/SystemAlert/src/main/java/ec/edu/espe/AlertSystemOfAccountingOffice/model/Addres.java
@@ -13,5 +13,39 @@ public class Addres {
     private String country;
     private String city;
     private String Street;
- 
+
+    public Addres(String country, String city, String Street) {
+        this.country = country;
+        this.city = city;
+        this.Street = Street;
+    }
+    
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return Street;
+    }
+
+    public void setStreet(String Street) {
+        this.Street = Street;
+    }
+
+    @Override
+    public String toString() {
+        return "Addres{" + "country=" + country + ", city=" + city + ", Street=" + Street + '}';
+    }
 }
