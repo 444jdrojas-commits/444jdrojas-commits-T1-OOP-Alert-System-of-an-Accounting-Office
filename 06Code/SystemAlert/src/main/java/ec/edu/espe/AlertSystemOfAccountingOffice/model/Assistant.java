1ed7a7bd2e96241dd6e37bebd8a04dea7c2d38b2
PauloRamos24 <paulo.alramos24@gmail.com>
2025-11-02 22:08:02 -0500

    v0.1.11

--- 06Code/SystemAlert/src/main/java/ec/edu/espe/AlertSystemOfAccountingOffice/model/Assistant.java
+++ 06Code/SystemAlert/src/main/java/ec/edu/espe/AlertSystemOfAccountingOffice/model/Assistant.java
@@ -18,4 +18,68 @@ public class Assistant {
     private String email;
     private String user;
     private String password;
+
+    public Assistant(String name, Date birthDate, int phone, String email, String user, String password) {
+        this.name = name;
+        this.birthDate = birthDate;
+        this.phone = phone;
+        this.email = email;
+        this.user = user;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Assistant{" + "name=" + name + ", birthDate=" + birthDate + ", phone=" + phone + ", email=" + email + ", user=" + user + ", password=" + password + '}';
+    }
+
+
 }
