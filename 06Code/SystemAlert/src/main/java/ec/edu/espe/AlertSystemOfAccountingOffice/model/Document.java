a88666208f676f56aeccece221da6939b5e797cc
Josue David Rojas Peña <svp.jd.rojas@gmail.com>
2025-11-02 23:41:40 -0500

    v 0.1.15 SettersGetters

--- 06Code/SystemAlert/src/main/java/ec/edu/espe/AlertSystemOfAccountingOffice/model/Document.java
+++ 06Code/SystemAlert/src/main/java/ec/edu/espe/AlertSystemOfAccountingOffice/model/Document.java
@@ -14,4 +14,54 @@ public class Document {
     private String name;
     private Date reviewDay;
     private String details;
+
+    public Document(String typeDocument, String status, String name, Date reviewDay, String details) {
+        this.typeDocument = typeDocument;
+        this.status = status;
+        this.name = name;
+        this.reviewDay = reviewDay;
+        this.details = details;
+    }
+
+    public void setTypeDocument(String typeDocument) {
+        this.typeDocument = typeDocument;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setReviewDay(Date reviewDay) {
+        this.reviewDay = reviewDay;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+    
+    public String getTypeDocument() {
+        return typeDocument;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Date getReviewDay() {
+        return reviewDay;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+    
+    
 }
